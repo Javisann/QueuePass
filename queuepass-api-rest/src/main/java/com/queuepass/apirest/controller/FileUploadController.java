9d85626a60ca74602534b1b5e75f186a316c8056
Javisan <javiersanchezprieto04@gmail.com>
2024-05-03 18:03:46 +0200

    Implementado Storage System

---
+++ queuepass-api-rest/src/main/java/com/queuepass/apirest/controller/FileUploadController.java
@@ -0,0 +1,68 @@
+package com.queuepass.apirest.controller;
+
+import com.queuepass.apirest.error.storage.StorageFileNotFoundException;
+import com.queuepass.apirest.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<String>> listUploadedFiles(Model model) throws IOException {
+        List<String> fileUrls = storageService.loadAll()
+                .map(path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toUri().toString())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(fileUrls);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+
+        if (file == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+
+        if (!file.isEmpty()) {
+            storageService.store(file);
+            return "Subida de fichero exitosa";
+        }
+
+        return "Error al subir el archivo";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+}
