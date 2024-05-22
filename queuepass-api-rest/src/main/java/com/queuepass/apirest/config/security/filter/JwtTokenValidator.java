8c286cae803ed5491b1a8d8eb1f1e1311ed0c74a
javisan <javiersanchezprieto04@gmail.com>
2024-05-22 10:54:38 +0200

    Test

--- queuepass-api-rest/src/main/java/com/queuepass/apirest/config/security/filter/JwtTokenValidator.java
+++ queuepass-api-rest/src/main/java/com/queuepass/apirest/config/security/filter/JwtTokenValidator.java
@@ -20,38 +20,57 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Collection;
 
-public class JwtTokenValidator extends OncePerRequestFilter { //Se ejecuta el filtro por cada request que se solicite
-
+/**
+ * Clase que valida un token JWT en cada solicitud.
+ */
+public class JwtTokenValidator extends OncePerRequestFilter {
 
     private JwtUtils jwtUtils;
 
+    /**
+     * Constructor de la clase JwtTokenValidator.
+     * @param jwtUtils Objeto JwtUtils utilizado para validar el token JWT.
+     */
     public JwtTokenValidator(JwtUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
     }
 
+    /**
+     * Método que se ejecuta para filtrar cada solicitud.
+     * @param request Objeto HttpServletRequest que representa la solicitud.
+     * @param response Objeto HttpServletResponse que representa la respuesta.
+     * @param filterChain Objeto FilterChain utilizado para continuar con la cadena de filtros.
+     */
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
 
+        // Obtener el token JWT del encabezado de autorización
         String jwtToken = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (jwtToken != null) {
+            // Eliminar el prefijo "Bearer" del token JWT
             jwtToken = jwtToken.substring(7);
 
+            // Validar el token JWT utilizando la clase JwtUtils
             DecodedJWT decodedJWT = jwtUtils.validateToken(jwtToken);
 
+            // Extraer el nombre de usuario y las autoridades del token
             String username = jwtUtils.extractUsername(decodedJWT);
             String stringAuthorities = jwtUtils.getSpecificClaim(decodedJWT, "authorities").asString();
 
+            // Convertir las autoridades en una lista de GrantedAuthority (Es un objeto que entiende Spring)
             Collection<? extends GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(stringAuthorities);
 
+            // Crear un contexto de seguridad vacío y establecer la autenticación
             SecurityContext context = SecurityContextHolder.createEmptyContext();
             Authentication authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
             context.setAuthentication(authenticationToken);
             SecurityContextHolder.setContext(context);
-
         }
+
+        // Continuar con la cadena de filtros
         filterChain.doFilter(request, response);
     }
 }
